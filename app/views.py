b1404387e2ea17e638ecd85b22e5dbd9a03cfcdb
betty <betty.nabuuso@moringaschool.com>
2021-10-31 18:01:51 +0300

    commiting new files

--- app/views.py
+++ app/views.py
@@ -9,3 +9,14 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
