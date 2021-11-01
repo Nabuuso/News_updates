60f8ba5ac6796eec50f370ac7382368f2a56aa71
betty <betty.nabuuso@moringaschool.com>
2021-11-01 05:15:47 +0300

    request.py for api requests

--- app/request.py
+++ app/request.py
@@ -1,11 +1,19 @@
 from app import app
 import urllib.request,json
-from .models import news
+from .models import news, sources, entertainment
 
- News= news.News
+News= news.News
+Sources = sources.Sources
+Entertainment = entertainment.Entertainment
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
 news_base_url = app.config["NEWS_UPDATES_BASE_URL"]
+
+# Getting the sources base url
+sources_base_url = app.config["NEWS_SOURCE_BASE_URL"]
+
+# Getting the entertainment news base url
+entertainment_base_url = app.config["NEWS_ENTERTAINMENT_BASE_URL"]
