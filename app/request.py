55efd2f66e2818547fc6cf8e636b6cc0e120addc
betty <betty.nabuuso@moringaschool.com>
2021-11-01 05:41:57 +0300

    request.py for api requests

--- app/request.py
+++ app/request.py
@@ -17,3 +17,22 @@ sources_base_url = app.config["NEWS_SOURCE_BASE_URL"]
 
 # Getting the entertainment news base url
 entertainment_base_url = app.config["NEWS_ENTERTAINMENT_BASE_URL"]
+
+def get_news(world):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = news_base_url.format(world,api_key)
+    
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['news-articles']:
+            news_results_list = get_news_response['news-articles']
+            news_results = process_newsResults(news_results_list)
+
+
+    return news_results
