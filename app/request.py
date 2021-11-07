4fdd15b5be572ce690abccef97bd5520908431bc
betty <betty.nabuuso@moringaschool.com>
2021-11-07 20:26:12 +0300

    running application trial

--- app/request.py
+++ app/request.py
@@ -15,3 +15,23 @@ def configure_request(app):
     base_url_sources = app.config['NEWS_API_BASE_URL']
     base_url_articles = app.config['NEWS_API_ARTICLES_URL']
     api_key = app.config['NEWS_API_KEY']
+    
+def get_sources():
+    '''
+    Function that gets the json response to url request
+    '''
+    # get_news_url = base_url.format(category,api_key)              #new
+    get_sources_url= 'https://newsapi.org/v2/sources?apiKey=4c6387d3b12945de99414364a18d6452'
+    # print(get_source_url)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
